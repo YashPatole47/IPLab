61ed37671f169c6a5ea68b804677d47a2f87fcdc
Yash Patole <110448487+YashPatole47@users.noreply.github.com>
2022-11-09 16:56:24 +0530

    Update newclass.js

--- ReactEXPs/EXP6Components/newclass.js
+++ ReactEXPs/EXP6Components/newclass.js
@@ -1,20 +1,13 @@
-import React from 'react';
-import Fun from './fun';
+import React from "react";
 
-export default class newclass extends React.Component{
+export default class newclass extends React.Component
+{
     render()
     {
         return(
             <div>
-                <h1>This is Class Component</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>Department</li>
-                    <li>About US</li>
-                    <li>Contact US</li>
-                </ul>
-                <Fun />
+            <h1>This is the CLASS COMPONENT.</h1>
             </div>
         )
+
     }
-}
