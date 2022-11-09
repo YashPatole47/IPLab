dd62b7e189e784ed36765245a50b9aa7f47f4844
Yash Patole <110448487+YashPatole47@users.noreply.github.com>
2022-11-09 18:29:07 +0530

    Add files via upload

--- EXP10-11(Node)/EXP11NodeCalc.js
+++ EXP10-11(Node)/EXP11NodeCalc.js
@@ -1,13 +1,11 @@
-// first run this in terminal 
 // npm install basic-calculator-js
-// remove this 3 lines before showing
 
 const calculate = require("basic-calculator-js")
  
-var x = 50
-var y = 50
+var x = 50;
+var y = 50;
  
-console.log(calculate.multiply(50, 50));
-console.log(calculate.subtract(x, y));
-console.log(calculate.add(x,y));
-console.log(calculate.divide(x, y));
+console.log("Multiply: "+calculate.multiply(x,y));
+console.log("Subtract: "+calculate.subtract(x, y));
+console.log("Addition: "+calculate.add(x,y));
+console.log("Divide: "+calculate.divide(x, y));
